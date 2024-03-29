15c1edd251eba45248eeb5d2926979afd7d0a4d7
JAHID HASAN <developerjahid@gmail.com>
2024-03-29 20:18:23 +0600

    3.3 module done

--- Module3/3.3.ts
+++ Module3/3.3.ts
@@ -1,8 +1,42 @@
 {
-    //type guard
+  //type guard
+
+  // type of ----> type guard
+  type alphaNumaric = string | number;
+
+  const add = (
+    param1: alphaNumaric,
+    param2: alphaNumaric
+  ): alphaNumaric | undefined => {
+    if (typeof param1 === "number" && typeof param2 === "number") {
+      return param1 + param2;
+    } else {
+        return param1.toString()+ param2.toString()
+    }
+  };
+
+ const res1= add("2",5)
+ console.log(res1);
+  //
+
+  type normaluser= {
+    name: string
+  }
+  type adminUser={
+    name:string;
+    role:'admin'
+  }
+
+  const getUser=(user:normaluser|adminUser)=>{
     
+    if('role' in user){
+        console.log(`i AM TH E ADDMIN ${user.name}`)
+    }
+    else{
+            console.log(`I am not admin a normal suer ${user.name}`)
+    }
+  }
 
-
-    //
-
+  const admin :adminUser= getUser({name:'jahid' , role:'admin'})
+  const normal: normaluser= getUser({name:'oli'})
 }
